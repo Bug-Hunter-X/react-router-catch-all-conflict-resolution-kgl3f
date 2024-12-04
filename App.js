3faaf31f2fb96f7f8de6f419fc455fb286b2bfe5
BugHunter-X <askzenai@gmail.com>
2024-12-04 16:40:10 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,21 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route is causing issues */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function NoMatch() { return <h1>404 Not Found</h1>; }
+export default App;
+```
